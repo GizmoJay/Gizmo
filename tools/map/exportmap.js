6ca78ed83ba3174a96a0337deb8d5af9e01a8284
lemueldls <lemueld73@gmail.com>
2020-04-28 09:43:13 -0400

    🔧 Tools fixed for linting

---
+++ tools/map/exportmap.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+
+config = { debugLevel: "all", debug: true };
+
+const Log = require("../../server/js/util/log");
+const processMap = require("./processmap");
+const fs = require("fs");
+_ = require("underscore");
+
+log = new Log();
+
+class ExportMap {
+  constructor () {
+    const self = this;
+
+    self.source = process.argv[2];
+
+    if (!self.source) {
+      self.source = "data/map-refactor.json";
+    }
+
+    fs.exists(self.source, exists => {
+      if (!exists) {
+        log.error(`The file ${source} could not be found.`);
+        return;
+      }
+
+      fs.readFile(self.source, (error, file) => {
+        self.handleMap(JSON.parse(file.toString()));
+      });
+    });
+  }
+
+  handleMap (data) {
+    const self = this;
+    const worldClientJSON = "../../server/data/map/world_client.json";
+    const worldServerJSON = "../../server/data/map/world_server.json";
+    const clientMapJSON = "../../client-dist/data/maps/map.json";
+    const clientMapJS = "../../client-dist/data/maps/map.js";
+
+    const worldClient = self.parse(data, worldClientJSON, "client");
+
+    self.parse(data, worldServerJSON, "server");
+    self.parse(data, clientMapJSON, "info", worldClient);
+    self.parse(data, clientMapJS, "info", worldClient, true);
+
+    self.copyTilesets();
+  }
+
+  parse (data, destination, mode, worldClient, isJS) {
+    const self = this;
+    const map = processMap(data, { mode: mode });
+
+    if (worldClient) {
+      map.depth = worldClient.depth;
+    }
+
+    let mapString = JSON.stringify(map);
+
+    if (isJS) {
+      mapString = "let mapData = " + mapString;
+    }
+
+    fs.writeFile(destination, mapString, (error, file) => {
+      if (error) {
+        log.error("An error has occurred while writing map files.");
+      } else {
+        log.notice(`[${mode.format()}] Map saved at: ${destination}`);
+      }
+    });
+
+    return map;
+  }
+
+  copyTilesets () {
+    const self = this;
+    const source = "./data";
+    const destination = "../../client-dist/img/tilesets";
+
+    fs.readdir(source, (error, files) => {
+      if (error) {
+        log.error("Could not copy the tilesets...");
+        return;
+      }
+
+      _.each(files, file => {
+        if (file.startsWith("tilesheet-")) {
+          fs.copyFileSync(`${source}/${file}`, `${destination}/${file}`);
+        }
+      });
+
+      log.notice(`Finished copying tilesets to ${destination}/`);
+    });
+  }
+}
+
+String.prototype.format = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+String.prototype.startsWith = function (str) {
+  return str.length > 0 && this.substring(0, str.length) === str;
+};
+
+module.exports = ExportMap;
+
+new ExportMap();
---
+++ tools/map/helper.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const Log = require("../../server/js/util/log");
+    const worldClient = require("../../server/data/map/world_client");
+    const worldServer = require("../../server/data/map/world_server");
+
+class Helper {
+    constructor () {
+        const self = this;
+
+        self.width = worldServer.width;
+        self.height = worldServer.height;
+
+        self.getTileData(884, 103);
+
+        self.getTileData(883, 104);
+        self.getTileData(884, 104);
+        self.getTileData(885, 104);
+
+        // for (let i = 1; i < 5; i++)
+        //    for (let j = 1; j < 5; j++)
+        //        self.getTileData(9 + i, 91 + j);
+    }
+
+    getTileData (x, y) {
+        const self = this;
+            const index = self.gridPositionToIndex(x, y);
+
+        console.log(`"${index}": { "data": [${worldClient.data[index]}], "isColliding": ${worldClient.collisions.indexOf(index) > -1} },`);
+        // log.info(index + ' -- ' + worldClient.data[index]);
+    }
+
+    gridPositionToIndex (x, y) {
+        return (y * this.width) + x;
+    }
+
+    indexToGridPosition (tileIndex) {
+        const self = this;
+
+        tileIndex -= 1;
+
+        const x = self.getX(tileIndex + 1, self.width);
+            const y = Math.floor(tileIndex / self.width);
+
+        return {
+            x: x,
+            y: y
+        };
+    }
+
+    getX (index, width) {
+        if (index === 0) { return 0; }
+
+        return (index % width === 0) ? width - 1 : (index % width) - 1;
+    }
+}
+
+module.exports = Helper;
+
+function main () {
+    new Helper();
+}
+
+main();
